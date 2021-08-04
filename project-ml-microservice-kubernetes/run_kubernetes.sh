b78f4e4a81e09860bf8138944cfe49d81e5674f5
Ubuntu <ubuntu@ip-172-31-25-18.eu-west-2.compute.internal>
2021-08-04 14:38:38 +0000

    kubernetes working

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="jonrexmorris/predict"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-predict --image=$dockerpath
+kubectl expose deployment udacity-predict --type=NodePort --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward service/udacity-predict 8000:80
